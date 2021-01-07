120978ee5fdb1d545c13479c3dca559f918e9285
Anzhar <anzharnugraha50@gmail.com>
2021-01-07 20:02:26 +0700

    update

---
+++ app/src/main/java/com/example/tokoonlineturorial/activity/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tokoonlineturorial.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tokoonlineturorial.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}

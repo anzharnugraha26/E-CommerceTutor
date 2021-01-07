48eb45d043f54f0d58211ee5353a16350e7d8edd
Anzhar <anzharnugraha50@gmail.com>
2021-01-07 19:25:53 +0700

    update

--- app/src/main/java/com/example/tokoonlineturorial/adapter/AdapterProduk.kt
+++ app/src/main/java/com/example/tokoonlineturorial/adapter/AdapterProduk.kt
@@ -1,36 +1,32 @@
 package com.example.tokoonlineturorial.adapter
 
 import android.app.Activity
-import android.content.Intent
-import android.provider.SyncStateContract
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
-import androidx.constraintlayout.solver.state.State
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tokoonlineturorial.R
 import com.example.tokoonlineturorial.helper.Helper
 import com.example.tokoonlineturorial.model.Produk
-import com.google.gson.Gson
-import com.squareup.picasso.Picasso
-import kotlinx.android.synthetic.main.item_produk.view.*
 
-class AdapterProduk(var activity: Activity, var data: ArrayList<Produk>) :
-    RecyclerView.Adapter<AdapterProduk.Holder> (){
+class AdapterProduk(var data: ArrayList<Produk>) :
+    RecyclerView.Adapter<AdapterProduk.Holder>() {
 
     class Holder(view: View) : RecyclerView.ViewHolder(view) {
         val tvNama = view.findViewById<TextView>(R.id.tv_nama)
         val tvHarga = view.findViewById<TextView>(R.id.tv_harga)
-        val imgProduk = view.findViewById<TextView>(R.id.img_produk)
-        val layout = view.findViewById<TextView>(R.id.layout)
+        val imgProduk = view.findViewById<ImageView>(R.id.img_produk)
+        val layout = view.findViewById<CardView>(R.id.layout)
 
     }
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
         val view: View =
-            LayoutInflater.from(parent.context).inflate(R.layout.item_produk, parent, false)
+            LayoutInflater.from(parent.context).inflate(R.layout.item_produk , parent, false)
         return Holder(view)
     }
 
@@ -39,21 +35,11 @@ class AdapterProduk(var activity: Activity, var data: ArrayList<Produk>) :
     }
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
-        holder.tvNama.text = data[position].name
-        holder.tvHarga.text = Helper().gantiRupiah(data[position].harga)
-//        val image = "http://192.168.43.231/tokoonline/public/storage/produk/" + data[position].image
-//        Picasso.get()
-//            .load(image)
-//            .placeholder(R.drawable.product)
-//            .error(R.drawable.product)
-//            .into(holder.imgProduk)
-//
-//        holder.layout.setOnClickListener {
-//            val activiti = Intent(activity, DetailProdukActivity::class.java)
-//            val str = Gson().toJson(data[position], Produk::class.java)
-//            activiti.putExtra("extra", str)
-//            activity.startActivity(activiti)
-//        }
+
+        holder.tvNama.text = data[position].nama
+        holder.tvHarga.text = data[position].harga
+        holder.imgProduk.setImageResource(data[position].gambar)
+
 
     }
 
--- app/src/main/java/com/example/tokoonlineturorial/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/tokoonlineturorial/fragment/HomeFragment.kt
@@ -5,21 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager.widget.ViewPager
 import com.example.tokoonlineturorial.R
+import com.example.tokoonlineturorial.adapter.AdapterProduk
 import com.example.tokoonlineturorial.adapter.AdapterSlider
+import com.example.tokoonlineturorial.model.Produk
 
 
 class HomeFragment : Fragment() {
     lateinit var vpSlider: ViewPager
+    lateinit var rvProduk: RecyclerView
+    lateinit var rvProdukTerlaris: RecyclerView
+    lateinit var rvElektronik: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view : View = inflater.inflate(R.layout.fragment_home, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_home, container, false)
 
-        vpSlider =  view.findViewById(R.id.vp_slider)
+        vpSlider = view.findViewById(R.id.vp_slider)
+        rvProduk = view.findViewById(R.id.rv_product)
+        rvProdukTerlaris = view.findViewById(R.id.rv_productTerlaris)
+        rvElektronik = view.findViewById(R.id.rv_elektronik)
+
 
         val arrSlider = ArrayList<Int>()
         arrSlider.add(R.drawable.slider1)
@@ -29,8 +40,118 @@ class HomeFragment : Fragment() {
         val adapterSlider = AdapterSlider(arrSlider, activity)
         vpSlider.adapter = adapterSlider
 
+        val layoutManager = LinearLayoutManager(activity)
+        layoutManager.orientation = LinearLayoutManager.HORIZONTAL
+
+        val layoutManager2 = LinearLayoutManager(activity)
+        layoutManager2.orientation = LinearLayoutManager.HORIZONTAL
+
+        val layoutManager3 = LinearLayoutManager(activity)
+        layoutManager3.orientation = LinearLayoutManager.HORIZONTAL
+
+
+        rvProduk.adapter = AdapterProduk(arrProduk)
+        rvProduk.layoutManager = layoutManager
+
+        rvProdukTerlaris.adapter = AdapterProduk(arrProdukTerlaris)
+        rvProdukTerlaris.layoutManager = layoutManager2
+
+        rvElektronik.adapter = AdapterProduk(arrProdukElekro)
+        rvElektronik.layoutManager = layoutManager3
+
+
         return view
     }
+
+
+    val arrProduk: ArrayList<Produk>
+        get() {
+            val arr = ArrayList<Produk>()
+            val p1 = Produk()
+            p1.nama = "Hp Core A3"
+            p1.harga = "Rp. 5.000.000"
+            p1.gambar = R.drawable.hp_14_bs749tu
+
+
+            val p2 = Produk()
+            p2.nama = "Hp Core A3zsa"
+            p2.harga = "Rp. 5.000.000"
+            p2.gambar = R.drawable.hp_envy_13_aq0019tx
+
+            val p3 = Produk()
+            p3.nama = "Hp Core Asaasasas3"
+            p3.harga = "Rp. 5.000.000"
+            p3.gambar = R.drawable.hp_notebook_14_bs709tu
+
+            arr.add(p1)
+            arr.add(p2)
+            arr.add(p3)
+
+            return arr
+        }
+
+    val arrProdukTerlaris: ArrayList<Produk>
+        get() {
+            val arr = ArrayList<Produk>()
+            val p1 = Produk()
+            p1.nama = "HP 14_bs749tu"
+            p1.harga = "Rp.5.500.000"
+            p1.gambar = R.drawable.hp_14_bs749tu
+
+            val p2 = Produk()
+            p2.nama = "Hp Envy_13_aq0019tx"
+            p2.harga = "Rp.15.980.000"
+            p2.gambar = R.drawable.hp_pavilion_15_cx0056wm
+
+            val p3 = Produk()
+            p3.nama = "HP pavilion_13_an0006na"
+            p3.harga = "Rp.14.200.000"
+            p3.gambar = R.drawable.hp_pavilion_14_ce1507sa
+
+            val p4 = Produk()
+            p4.nama = "Hp Envy_13_aq0019tx"
+            p4.harga = "Rp.15.980.000"
+            p4.gambar = R.drawable.hp_pavilion_14_ce1507sa
+
+            arr.add(p4)
+            arr.add(p1)
+            arr.add(p3)
+            arr.add(p2)
+
+            return arr
+        }
+
+
+    val arrProdukElekro: ArrayList<Produk>
+        get() {
+            val arr = ArrayList<Produk>()
+            val p1 = Produk()
+            p1.nama = "HP 14_bs749tu"
+            p1.harga = "Rp.5.500.000"
+            p1.gambar = R.drawable.hp_14_bs749tu
+
+            val p2 = Produk()
+            p2.nama = "Hp Envy_13_aq0019tx"
+            p2.harga = "Rp.15.980.000"
+            p2.gambar = R.drawable.hp_pavilion_15_cx0056wm
+
+            val p3 = Produk()
+            p3.nama = "HP pavilion_13_an0006na"
+            p3.harga = "Rp.14.200.000"
+            p3.gambar = R.drawable.hp_pavilion_14_ce1507sa
+
+            val p4 = Produk()
+            p4.nama = "Hp Envy_13_aq0019tx"
+            p4.harga = "Rp.15.980.000"
+            p4.gambar = R.drawable.hp_pavilion_14_ce1507sa
+
+            arr.add(p4)
+            arr.add(p1)
+            arr.add(p3)
+            arr.add(p2)
+
+            return arr
+        }
 
 
 }

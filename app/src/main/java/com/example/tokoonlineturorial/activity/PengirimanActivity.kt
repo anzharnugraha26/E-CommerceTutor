ca03708739362255fabbffd828fdc1a0c8314b33
anzharnugraha26 <anzharnugraha50@gmail.com>
2021-03-09 14:35:51 +0700

    update

--- app/src/main/java/com/example/tokoonlineturorial/activity/PengirimanActivity.kt
+++ app/src/main/java/com/example/tokoonlineturorial/activity/PengirimanActivity.kt
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.tokoonlineturorial.R
 import com.example.tokoonlineturorial.adapter.AdapterKurir
@@ -19,21 +21,57 @@ import com.example.tokoonlineturorial.util.Apikey
 import kotlinx.android.synthetic.main.activity_pengiriman.*
 import kotlinx.android.synthetic.main.activity_pengiriman.btn_tambahAlamat
 import kotlinx.android.synthetic.main.activity_pengiriman.div_kosong
+import kotlinx.android.synthetic.main.activity_tambah_alamat.*
 import kotlinx.android.synthetic.main.toolbar.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class PengirimanActivity : AppCompatActivity() {
-    lateinit var  myDb : MyDatabase
+    lateinit var myDb: MyDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pengiriman)
         Helper().setToolBar(this, toolbar, "Pengiriman")
         myDb = MyDatabase.getInstance(this)!!
+        setSpinner()
 
 
         mainButton()
+    }
+
+    private fun setSpinner() {
+        val arryString = ArrayList<String>()
+        arryString.add("JNE")
+        arryString.add("POS")
+        arryString.add("TIKI")
+        val adapter = ArrayAdapter<Any>(
+            this,
+            R.layout.item_spiner,
+            arryString.toTypedArray()
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spn_kurir.adapter = adapter
+
+        spn_kurir.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    if (position != 0) {
+                        getOngkir(spn_kurir.selectedItem.toString())
+                    }
+
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+
+                }
+
+            }
     }
 
     @SuppressLint("SetTextI18n")
@@ -42,7 +80,6 @@ class PengirimanActivity : AppCompatActivity() {
         if (myDb.daoAlamat().geByStatus(true) != null) {
             div_alamat.visibility = View.VISIBLE
             div_kosong.visibility = View.GONE
-            tv_kosong.visibility = View.GONE
 
             div_metodePengiriman.visibility = View.VISIBLE
 
@@ -53,12 +90,11 @@ class PengirimanActivity : AppCompatActivity() {
                 a.alamat + ", " + a.kota + ", " + a.kecamatan + ", " + a.kodepos + ", (" + a.type + ")"
             btn_tambahAlamat.text = "Ubah Alamat"
 
-            getOngkir()
+            getOngkir("JNE")
         } else {
             div_alamat.visibility = View.GONE
             div_kosong.visibility = View.VISIBLE
             btn_tambahAlamat.text = "Tambah Alamat"
-            tv_kosong.visibility = View.GONE
         }
     }
 
@@ -68,25 +104,27 @@ class PengirimanActivity : AppCompatActivity() {
         }
     }
 
-    private fun getOngkir() {
+    private fun getOngkir(kurir: String) {
         val alamat = myDb.daoAlamat().geByStatus(true)
         val origin = "501"
         val destination = "" + alamat!!.idKota
         val berat = 100
-        val kurir = "jne"
 
-        ApiConfigAlamat.instanceRetrofit.ongkir(Apikey.key, origin, destination , berat, kurir)
+        ApiConfigAlamat.instanceRetrofit.ongkir(Apikey.key, origin, destination, berat, kurir.toLowerCase())
             .enqueue(object : Callback<ResponseOngkir> {
                 override fun onFailure(call: Call<ResponseOngkir>, t: Throwable) {
                     Log.d("error", "gagal memuat data" + t.message)
                 }
 
-                override fun onResponse(call: Call<ResponseOngkir>, response: Response<ResponseOngkir>) {
+                override fun onResponse(
+                    call: Call<ResponseOngkir>,
+                    response: Response<ResponseOngkir>
+                ) {
                     if (response.isSuccessful) {
                         Log.d("Success", "berhasil memuat data" + response.message())
                         val result = response.body()!!.rajaongkir.results
-                        if (result.isNotEmpty()){
-                            displayOngkir( result[0].code.toUpperCase() ,  result[0].costs)
+                        if (result.isNotEmpty()) {
+                            displayOngkir(result[0].code.toUpperCase(), result[0].costs)
                         }
 
 
@@ -97,10 +135,11 @@ class PengirimanActivity : AppCompatActivity() {
                 }
             })
     }
-    private fun displayOngkir(kurir: String , arrayList: ArrayList<Costs>) {
+
+    private fun displayOngkir(kurir: String, arrayList: ArrayList<Costs>) {
         val layaoutManager = LinearLayoutManager(this)
         layaoutManager.orientation = LinearLayoutManager.VERTICAL
-        rv_metode.adapter = AdapterKurir( arrayList, kurir ,object : AdapterKurir.Listeners{
+        rv_metode.adapter = AdapterKurir(arrayList, kurir, object : AdapterKurir.Listeners {
 
             override fun onClicked(data: Alamat) {
 
